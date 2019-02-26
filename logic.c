fdf200ff2a0c15845f321a52f074e06994868655
Jacob Allen <jballen95173@gmail.com>
2019-02-26 14:17:51 -0500

    Initial commit

---
+++ logic.c
@@ -0,0 +1,21 @@
+#include "logic.h"
+#include "graphics.h"
+#include "gba.h"
+#include <stdlib.h>
+//extern volatile OamEntry* shadow;
+void initializeAppState(AppState* appState) {
+    // TA-TODO: Initialize everything that's part of this AppState struct here.
+    // Suppose the struct contains random values, make sure everything gets
+    // the value it should have when the app begins.
+    
+}
+
+
+
+// This function processes your current app state and returns the new (i.e. next)
+// state of your application.
+AppState processAppState(AppState *currentAppState, u32 keysPressedBefore, u32 keysPressedNow) {
+    AppState nextAppState = *currentAppState;
+    
+    return nextAppState;
+}
